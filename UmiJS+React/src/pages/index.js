159f89cca4cfee889e33bc5bda19dd7d04e6c6e3
hocgin <hocgin@gmail.com>
2018-11-14 21:57:31 +0800

    + 路由

--- UmiJS+React/src/pages/index.js
+++ UmiJS+React/src/pages/index.js
@@ -1,13 +1,16 @@
 import styles from './index.css';
+import Link from 'umi/link';
+import Redirect from 'umi/redirect';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+export default function () {
+    return (
+        <div className={styles.normal}>
+            <div className={styles.welcome}/>
+            <ul className={styles.list}>
+                <Link to={`/test`}>测试</Link>
+                <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+                <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
+            </ul>
+        </div>
+    );
 }
--- UmiJS+React/src/pages/test/index.js
+++ UmiJS+React/src/pages/test/index.js
@@ -1,1 +1,11 @@
 import style from './index.css';
+import Link from "umi/link";
+import Redirect from "umi/redirect";
+export default function () {
+    return (
+        <div>测试文本
+            <Link to={`/`}>回去</Link>
+            {/*<Redirect to={`/`}/>*/}
+        </div>
+    );
+}

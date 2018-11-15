ccbcbe5255b19e4f045bf30b93160f438e59dc3f
hocgin <hocgin@gmail.com>
2018-11-16 00:04:10 +0800

    ~

--- UmiJS+React/src/components/SearchBar/index.js
+++ UmiJS+React/src/components/SearchBar/index.js
@@ -1,18 +1,89 @@
 import React, {Component} from 'react';
+import router from 'umi/router';
 import classNames from 'classnames';
 import styles from './index.less';
 
 export default class SearchBar extends Component {
     state = {
         keywords: '',
-        tabNames: ['Google', 'Baidu', 'Yahoo', '搜狗'],
-        classify: {
-            clicked: '全部',
-            list: ['全部', '音乐', '图片', '视频']
-        },
         tab: {
-          clicked: 0,
-          list: ['Google', 'Baidu', 'Yahoo', '搜狗']
+            selected: 0,
+            options: [{
+                name: 'Google',
+                classify: {
+                    selected: 0,
+                    options: [{
+                        name: '全部',
+                        link: 'https://www.google.com/search?q='
+                    }, {
+                        name: '图片',
+                        link: 'https://www.google.com/search?tbm=isch&q='
+                    }, {
+                        name: '视频',
+                        link: 'https://www.google.com/search?tbm=vid&q='
+                    }, {
+                        name: '新闻',
+                        link: 'https://www.google.com/search?tbm=nws&q='
+                    }]
+                }
+            }, {
+                name: 'Bing',
+                classify: {
+                    selected: 0,
+                    options: [{
+                        name: '国际',
+                        link: 'https://www.google.com/search?ensearch=1&q='
+                    }, {
+                        name: '国内',
+                        link: 'https://www.google.com/search?q='
+                    }]
+                }
+            }, {
+                name: 'Baidu',
+                classify: {
+                    selected: 0,
+                    options: [{
+                        name: '全部',
+                        link: 'https://www.baidu.com/s?wd='
+                    }, {
+                        name: '知道',
+                        link: 'https://zhidao.baidu.com/search?ct=17&word='
+                    }, {
+                        name: '图片',
+                        link: 'http://image.baidu.com/search/index?ct=201326592&word='
+                    }, {
+                        name: '视频',
+                        link: 'https://wenku.baidu.com/search?lm=0&word='
+                    }]
+                }
+            }, {
+                name: '淘宝',
+                classify: {
+                    selected: 0,
+                    options: [{
+                        name: '全部',
+                        link: 'https://s.taobao.com/search?q='
+                    }]
+                }
+            }, {
+                name: '搜狗',
+                classify: {
+                    selected: 0,
+                    options: [{
+                        name: '全部',
+                        link: 'https://www.sogou.com/web?query='
+                    }, {
+                        name: '图片',
+                        link: 'https://pic.sogou.com/pics?query='
+                    }, {
+                        name: '微信',
+                        link: 'https://weixin.sogou.com/weixin?query='
+                    }, {
+                        name: '知乎',
+                        link: 'https://zhihu.sogou.com/zhihu?query='
+                    }]
+                }
+            }]
         }
     };
 
@@ -21,42 +92,46 @@ export default class SearchBar extends Component {
     }
 
 
-    renderTabs(tabs = this.state.tab.list,
-               clicked = this.state.tab.clicked) {
-        return tabs.map((tab, i) => {
+    renderTabs() {
+        const {options, selected} = this.state.tab;
+
+        return options.map(option => {
+            return option.name;
+        }).map((name, i) => {
             return (
                 <div key={i} className={classNames(
                     styles.link,
-                    {[styles.active]: i === clicked}
+                    {[styles.active]: i === selected}
                 )}>
-                    <a href="#">{tab}</a>
+                    <a href="#" onClick={this.handleChooseEngine.bind(this, i)}>{name}</a>
                 </div>
             );
         });
     }
 
     renderClassifies() {
-        let {classify} = this.state;
-        let content = <button key='0' className={styles.drop_btn}>{classify.clicked}</button>;
+        let tab = this.state.tab;
+        let {options, selected} = tab.options[tab.selected].classify;
+        let content = (
+            <button key='0' className={styles.drop_btn}>
+                {options[selected].name}
+            </button>
+        );
 
-        let items = (
+        let items = options.length > 1 && (
             <div key='1' className={styles.dropdown_content}>
-                {classify.list.map((classify, i) => {
-                    return (
-                        <a key={i} href="#" onClick={this.handleChooseClassify.bind(this, i)}>{classify}</a>
-                    );
-                })}
+                {
+                    options.map(option => option.name)
+                        .map((name, i) => {
+                            return (
+                                <a key={i} href="#" onClick={this.handleChooseClassify.bind(this, i)}>{name}</a>
+                            );
+                        })
+                }
             </div>
         );
 
         return [content, items]
-    }
-
-    handleChooseClassify(i) {
-        const {list} = this.state.classify;
-        this.setState({
-            classify: Object.assign(this.state.classify, {clicked: list[i]})
-        });
     }
 
     render() {
@@ -73,10 +148,10 @@ export default class SearchBar extends Component {
                            style={{fontSize: "1.03em"}}
                            value={this.state.keywords}
                            onChange={(v) => {
-                               this.setState({keywords: v.target.value})
+                               this.setState({keywords: v.target.value});
                            }}
                     />
-                    <button className={styles.search_btn}>
+                    <button className={styles.search_btn} onClick={this.handleSearch.bind(this)}>
                             <span className={styles.icon}>
                                 <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path
@@ -87,5 +162,35 @@ export default class SearchBar extends Component {
                 </div>
             </div>
         );
+    }
+
+    /**
+     * ########################################
+     *                  绑定事件
+     * ########################################
+     */
+    handleChooseClassify(i) {
+        let {options, selected} = this.state.tab;
+        this.setState({
+            [options[selected].classify]: Object.assign(options[selected].classify, {
+                selected: i
+            })
+        });
+    }
+
+    handleChooseEngine(i) {
+        this.setState({
+            tab: Object.assign(this.state.tab, {selected: i})
+        });
+    }
+
+    handleSearch() {
+        const tab = this.state.tab,
+            {options, selected} = tab.options[tab.selected].classify;
+        const q = this.state.keywords;
+        if (q.length === 0) {
+            return;
+        }
+        window.location.href = `${options[selected].link}${q}`;
     }
 };
--- UmiJS+React/src/pages/index.js
+++ UmiJS+React/src/pages/index.js
@@ -2,19 +2,12 @@ import styles from './index.css';
 import Link from 'umi/link';
 import SearchBar from '../components/SearchBar';
 import Redirect from 'umi/redirect';
+import React from "react";
 
 export default function () {
     return (
         <div className={styles.normal}>
-            <SearchBar></SearchBar>
-            <br/>
-            <br/>
-            <br/>
-            <ul className={styles.list}>
-                <Link to={`/test`}>测试</Link>
-                <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-                <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-            </ul>
+            <SearchBar/>
         </div>
     );
 }

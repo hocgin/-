5aaff3dd8062e80c71e63adcdae2fee877ddad00
hocgin <hocgin@gmail.com>
2018-11-18 23:11:14 +0800

    ~ 虚拟键盘

--- UmiJS+React/src/components/HotKey/index.js
+++ UmiJS+React/src/components/HotKey/index.js
@@ -5,31 +5,29 @@ import Key from '../Key';
 
 export default class HotKey extends Component {
 
+    state = {
+        keys: [
+            ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+            ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+            ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+        ]
+    };
+
 
     render() {
         return (
             <div className={classNames(this.props.className, styles.hot_key)}>
-                <div>
-                    {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((i) => {
-                        return (
-                            <Key className={styles.key} value={`${i}`}/>
-                        );
-                    })}
-                </div>
-                <div>
-                    {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].map((i) => {
-                        return (
-                            <Key className={styles.key} value={`${i}`}/>
-                        );
-                    })}
-                </div>
-                <div>
-                    {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((i) => {
-                        return (
-                            <Key className={styles.key} value={`${i}`}/>
-                        );
-                    })}
-                </div>
+                {this.state.keys.map((item) => {
+                    return (
+                        <div>
+                            {item.map((i) => {
+                                return (
+                                    <Key className={styles.key} value={`${i}`}/>
+                                );
+                            })}
+                        </div>
+                    )
+                })}
             </div>
         );
     }

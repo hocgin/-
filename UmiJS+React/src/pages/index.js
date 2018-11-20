ea9de3422ee58786528a6200fa118af1d1355540
hocgin <hocgin@gmail.com>
2018-11-20 09:27:41 +0800

    + 滚轮事件

--- UmiJS+React/src/pages/index.js
+++ UmiJS+React/src/pages/index.js
@@ -3,88 +3,89 @@ import SearchBar from '../components/SearchBar';
 import Toolbar from '../components/Toolbar';
 import Card from '../components/Card';
 import HotKey from '../components/HotKey';
-import Todo from '../components/Todo';
-import React from "react";
-import {Icon, Row, Col} from 'antd';
+import React, {Component} from "react";
+import {Col, Icon, Row} from 'antd';
 
 
-function section1() {
+export default class Page extends Component {
+
+  state = {
+    h: '-100vh'
+  };
+
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  handleScroll(e) {
+    console.log('-->', e);
+  }
+
+  render() {
     return (
-        <div className={styles.normal}>
-            <Toolbar className={styles.toolbar}/>
-            <div className={styles.header}>
-                <SearchBar className={styles.search_bar}/>
-                <HotKey className={styles.hot_key}/>
-            </div>
-            <div className={styles.body}>
-                <div className={styles.site}>
-                    <div className={styles.content_container}>
-                        <div className={styles.title}>
-                            <h3>推荐</h3>
-                        </div>
-                        <Row>
-                            {
-                                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
-                                    return (
-                                        <Col xl={4} className={styles.item}>
-                                            <Card/>
-                                        </Col>
-                                    );
-                                })
-                            }
-                        </Row>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.footer}>
-                <div className={styles.footer_outer}>
-                    <div className={styles.footer_inner}>
-                        © 2018
-                        <Icon className="heartbeat" type="heart" theme="filled"
-                              style={{color: '#F22E3C', margin: '0 5px'}}/>
-                        <a className="me" href="https://hocg.in" target="_blank">hocgin</a>.
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default function () {
-    // return (
-    //     // OPEN-SOURCE-GPLV3-LICENSE
-    //     <ReactFullpage
-    //
-    //         render={({state, fullpageApi}) => {
-    //             return (
-    //                 <ReactFullpage.Wrapper>
-    //                     <div className="section">
-    //                         <p>Section 2</p>
-    //                     </div>
-    //                     <div className="section">
-    //                         {section1()}
-    //                     </div>
-    //                 </ReactFullpage.Wrapper>
-    //             );
-    //         }}
-    //     />
-    // );
-    return (
+      <div style={{
+        transform: `translate3d(0px, ${this.state.h}, 0px)`,
+        transition: 'all 700ms ease 0s',
+        position: 'absolute',
+        width: '100%',
+        touchAction: 'none'
+      }}>
         <div style={{
-            transform: 'translate3d(0px, -100vh, 0px)',
-            transition: 'all 700ms ease 0s',
-            position: 'absolute',
-            width: '100%',
-            touchAction: 'none'
-        }}>
-            <div style={{
-                height: '100vh',
-                backgroundColor: 'gray'
-            }}>1
-            </div>
-            <div>
-                {section1()}
-            </div>
+          height: '100vh',
+          backgroundColor: 'gray'
+        }}>1
         </div>
-    )
+        <div onClick={this.handleClick.bind(this)}>
+          {this.renderSection1()}
+        </div>
+      </div>
+    );
+  }
+
+  handleClick() {
+    this.setState({h: '0vh'});
+  }
+
+  renderSection1() {
+    return (
+      <div className={styles.normal}>
+        <Toolbar className={styles.toolbar}/>
+        <div className={styles.header}>
+          <SearchBar className={styles.search_bar}/>
+          <HotKey className={styles.hot_key}/>
+        </div>
+        <div className={styles.body}>
+          <div className={styles.site}>
+            <div className={styles.content_container}>
+              <div className={styles.title}>
+                <h3>推荐</h3>
+              </div>
+              <Row>
+                {
+                  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
+                    return (
+                      <Col xl={4} className={styles.item}>
+                        <Card/>
+                      </Col>
+                    );
+                  })
+                }
+              </Row>
+            </div>
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.footer_outer}>
+            <div className={styles.footer_inner}>
+              © 2018
+              <Icon className="heartbeat" type="heart" theme="filled"
+                    style={{color: '#F22E3C', margin: '0 5px'}}/>
+              <a className="me" href="https://hocg.in" target="_blank">hocgin</a>.
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }

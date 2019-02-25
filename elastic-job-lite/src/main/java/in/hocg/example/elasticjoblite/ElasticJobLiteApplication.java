69192dc57c9e33076cffc3e06af29b1c8d12c6b0
hongchengjin <hocgin123>
2019-02-25 11:30:10 +0800

    ejob demo

---
+++ elastic-job-lite/src/main/java/in/hocg/example/elasticjoblite/ElasticJobLiteApplication.java
@@ -0,0 +1,13 @@
+package in.hocg.example.elasticjoblite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticJobLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobLiteApplication.class, args);
+    }
+
+}
